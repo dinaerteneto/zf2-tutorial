40799c14b9352a34a2956d163986686f4fe63fe6
Dinaerte Neto <dinaerteneto@gmail.com>
2014-12-13 12:21:18 -0200

    Listando os álbuns
    Lista de álbuns

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -1,11 +1,11 @@
 <?php
+
 return array(
     'controllers' => array(
         'invokables' => array(
-            'Album\Controllers\Album' => 'Album\Controller\AlbumController'
-        )
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
     ),
-    
     // The following section is new and should be added to your file
     'router' => array(
         'routes' => array(
@@ -14,8 +14,8 @@ return array(
                 'options' => array(
                     'route' => '/album[/][:action][/:id]',
                     'constraints' => array(
-                        'action' => '[a-zA-z][a-zA-Z0-9_-]*',
-                        'id' => '[0-9]+'
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
                     ),
                     'defaults' => array(
                         'controller' => 'Album\Controller\Album',
@@ -25,7 +25,6 @@ return array(
             )
         )
     ),
-    
     'view_manager' => array(
         'template_path_stack' => array(
             'album' => __DIR__ . '/../view'
